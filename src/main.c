6fd5aaa925614f3f0cdfac6bd4024c2fc9dbb9ab
AlexandreLanglade <alexandre.langlade@outlook.fr>
2019-12-11 17:00:01 +0100

    conception V0.2.1

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int main(int argc, char const *argv[])
+{
+    /*
+    verif indexation : nb element / config modifiee
+    ->indexation des non-indexés !
+
+
+    */
+    return 0;
+}
