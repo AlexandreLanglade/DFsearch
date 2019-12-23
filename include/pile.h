cbb94582a203279a1c4b60f8928afdb3fd7b7a0d
AlexandreLanglade <alexandre.langlade@outlook.fr>
2019-12-23 13:51:50 +0100

    pile V0.9

--- include/pile.h
+++ include/pile.h
@@ -2,8 +2,8 @@
  * @file pile.h
  * @author LANGLADE Alexandre
  * @brief Pile générique pour les descripteurs
- * @version 0.1
- * @date 2019-12-15
+ * @version 0.9
+ * @date 2019-12-23
  * 
  * @copyright Copyright (c) 2019
  * 
@@ -12,17 +12,16 @@
 #ifndef H_PILE
 #define H_PILE
 
-#include <stdio.h>
-#include <stdlib.h>
 #include "include/descripteur_texte.h"
 #include "include/descripteur_image.h"
 #include "include/descripteur_audio.h"
 
-
 typedef struct pile_s * Pile;
 
-//init
-//empiler
-//depiler x3
+Pile initialiser_pile(int type);
+void empiler_pile(Pile p, void * elt_descripteur, int type);
+int estVide_pile(Pile p);
+void * depiler_pile(Pile p, int type);
+void * top_pile(Pile p, int type);
 
 #endif
--- src/pile.c
+++ src/pile.c
@@ -2,21 +2,130 @@
  * @file pile.c
  * @author LANGLADE Alexandre
  * @brief Pile générique pour les descripteurs
- * @version 0.1
- * @date 2019-12-15
+ * @version 0.9
+ * @date 2019-12-23
  * 
  * @copyright Copyright (c) 2019
  * 
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "include/pile.h"
 
+/**
+ * @brief Structure d'une pile générique
+ * 
+ */
 struct pile_s
 {
-    int texte;
-    int image;
-    int audio;
+    int type; /* 1texte 2image 3audio */
     Descripteur_texte dt;
     Descripteur_image di;
     Descripteur_audio da;
 };
+
+/**
+ * @brief Fonction qui initialise une pile vide
+ * 
+ * @param type Type des descripteurs dans la pile (1:texte/2:image/3:audio)
+ * @return Pile vide
+ */
+Pile initialiser_pile(int type)
+{
+    assert(type > 0 && type <= 3);
+    Pile res = (Pile)malloc(sizeof(struct pile_s));
+    assert(res != NULL);
+    res->dt = NULL;
+    res->di = NULL;
+    res->da = NULL;
+    res->type = type;
+    return res;
+}
+
+/**
+ * @brief Fonction ajoutant un descripteur à la pile
+ * 
+ * @param p La pile sur laquelle empiler
+ * @param elt_descripteur Le descripteur à empiler
+ * @param type Le type des descripteur de la pile (1:texte/2:image/3:audio)
+ */
+void empiler_pile(Pile p, void * elt_descripteur, int type)
+{
+    assert(p->type == type);
+    if (p->type == 1)
+    {
+        if (p->dt != NULL) setDtSuiv(elt_descripteur, p->dt);
+        p->dt = elt_descripteur;
+    } else if (p->type == 2)
+    {
+        if (p->di != NULL) setDiSuiv(elt_descripteur, p->di);
+        p->di = elt_descripteur;
+    } else
+    {
+        if (p->da != NULL) setDaSuiv(elt_descripteur, p->da);
+        p->da = elt_descripteur;
+    }  
+}
+
+/**
+ * @brief Fonction indiquant si la pile est vide ou non
+ * 
+ * @param p La pile à regarder
+ * @return int 
+ */
+int estVide_pile(Pile p)
+{
+    return (p->dt == NULL && p->di == NULL && p->da == NULL);
+}
+
+/**
+ * @brief Fonction retirant le descripteur au sommet de la pile
+ * 
+ * @param p La pile que l'on souhaite dépiler
+ * @param type Le type des descripteur de la pile (1:texte/2:image/3:audio)
+ * @return void* Le descripteur qui était au sommet de la pile
+ */
+void * depiler_pile(Pile p, int type)
+{
+    assert(p->type == type);
+    assert(!estVide_pile(p));
+    void * aux;
+    if (p->type == 1)
+    {
+        aux = p->dt;
+        p->dt = getDtSuiv(p->dt);
+    } else if (p->type == 2)
+    {
+        aux = p->di;
+        p->di = getDiSuiv(p->di);
+    } else
+    {
+        aux = p->da;
+        p->da = getDaSuiv(p->da);
+    }
+    return aux;    
+}
+
+/**
+ * @brief Fonction renvoyant le descripteur au sommet de la pile
+ * 
+ * @param p La pile que l'on souhaite traiter
+ * @param type Le type des descripteur de la pile (1:texte/2:image/3:audio) 
+ * @return void* Le descripteur au sommet de la pile
+ */
+void * top_pile(Pile p, int type)
+{
+    assert(p->type == type);
+    assert(!estVide_pile(p));
+    if (p->type == 1)
+    {
+        return p->dt;
+    } else if (p->type == 2)
+    {
+        return p->di;
+    } else
+    {
+        return p->da;
+    }  
+}
